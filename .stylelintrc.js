26ebd3028030faa518d47a53c699802cabe11209
KG <kimkiwonn@gmail.com>
2024-02-09 15:20:33 +0900

    [#1] chore: add stylelint config

---
+++ .stylelintrc.js
@@ -0,0 +1,23 @@
+module.export = {
+  processors: ['stylelint-processor-styled-components'],
+  extends: ['stylelint-config-recommended', 'stylelint-config-styled-components'],
+  syntax: 'scss',
+  rules: {
+    'block-no-empty': null,
+    'at-rule-no-unknown': null,
+    'color-hex-case': 'lower',
+    'function-name-case': null,
+    'declaration-colon-newline-after': null,
+    'selector-pseudo-element-colon-notation': 'double',
+    'selector-pseudo-class-no-unknown': null,
+    'value-no-vendor-prefix': null,
+    'value-keyword-case': null,
+    'property-no-vendor-prefix': null,
+    'no-descending-specificity': null,
+    'rule-empty-line-before': null,
+    'function-comma-newline-after': null,
+    'no-eol-whitespace': null,
+    'function-parentheses-newline-inside': null,
+  },
+  ignoreFiles: ['./public/**/*.css'],
+}

b0945977cfc7b8596eed9af02919bb9a64feb655
KG <kimkiwonn@gmail.com>
2024-02-09 15:17:58 +0900

    [#1] chore: add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 120,
+  tabWidth: 2,
+  semi: false,
+  useTabs: false,
+  bracketSpacing: true,
+  bracketSameLine: true,
+}

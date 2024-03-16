d7c23baaf7684544b952f2d2b020c2a9eab28e5b
KG <kimkiwonn@gmail.com>
2024-03-16 16:27:56 +0900

    feat: 사이드바에 사용자 정보 추가

--- src/app/_components/Sidebar/Sidebar.tsx
+++ src/app/_components/Sidebar/Sidebar.tsx
@@ -15,8 +15,14 @@ export default function Sidebar() {
         id="default-sidebar"
         className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
         aria-label="Sidebar">
-        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-          <ul className="space-y-2 font-medium">
+        <div className="flex flex-col items-center h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+          <div className="py-10 text-center h-1/4 space-y-1.5">
+            <p className="text-xl">김기원</p>
+            <p className="text-sm">Lv. 1</p>
+            <p className="text-sm">10 / 10000</p>
+            <p className="text-sm">0 / 9 (일일 퀘스트)</p>
+          </div>
+          <ul className="space-y-7 font-medium h-3/4">
             <SidebarElement
               name="Home"
               link="/"
